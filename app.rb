396422309fa628038ebf6006287e65f3d4076b72
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2020-04-21 19:27:54 +0200

    feat: add backend

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'json'
+
+post '/event_handler' do
+  @payload = JSON.parse(params[:payload])
+
+  case request.env['HTTP_X_GITHUB_EVENT']
+  when "pull_request"
+    if @payload["action"] == "closed" && @payload["pull_request"]["merged"]
+      puts "A pull request was merged! A deployment should start now..."
+    end
+  end
+end
